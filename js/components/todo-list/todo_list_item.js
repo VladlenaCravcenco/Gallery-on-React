1621d4056c5fa5d4a0a10239a04a2c42ea6eca05
VladlenaCravcenco <61877933+VladlenaCravcenco@users.noreply.github.com>
2023-01-26 09:59:27 +0200

    Create todo_list_item.js

---
+++ js/components/todo-list/todo_list_item.js
@@ -0,0 +1,70 @@
+import React, { Component } from "react";
+import './todo-list.css';
+
+export default class TodoListItem extends Component {
+  
+    state = {
+        done: false,
+important: false
+    };
+    
+    onLabelClick = () => {
+        this.setState(({done}) => {
+            return {
+               done: !done
+            }
+        });
+    }; 
+
+    onMarkImportant = () => {
+   this.setState(({important}) => {
+    return{
+      important: !important  
+    } 
+   })
+    };
+
+   
+
+    render() {
+
+        const { label, onDeleted } = this.props;
+        const { done, important} = this.state;
+
+        let classNames = 'todo-list-item';
+        if (done) {
+            classNames += ' done';
+        }
+
+        if (important){
+            classNames += ' important';
+        }
+
+        
+        return (
+            <span className={classNames}>
+                <span
+                    className="todo-list-item-label"
+                
+                    onClick={this.onLabelClick}>
+                    {label}
+                    <span>
+                        <button type="button" 
+                        className="btn btn-outline-success btn-sm " 
+                        onClick={this.onMarkImportant}>
+                            <i className="fa fa-exclamation"></i>
+                            </button>
+                       
+                        <button type="button" 
+                        className="btn btn-outline-danger btn-sm "
+                        onClick={onDeleted}>
+                            <i className="fa fa-trash-o"></i>
+                            </button>
+                    </span>
+                </span>
+            </span>
+        )
+
+    }
+}
+
