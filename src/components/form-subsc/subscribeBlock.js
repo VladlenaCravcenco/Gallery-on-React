89054311c755dc5885a703a29bbfbe4ff6c753da
Vlada <cravcenco97@inbox.ru>
2023-05-14 14:47:05 +0300

    parallax

--- src/components/form-subsc/subscribeBlock.js
+++ src/components/form-subsc/subscribeBlock.js
@@ -1,32 +1,33 @@
 import React, { Component } from 'react';
 
-class SubscribeBlock extends Component {
+
+export default class SubscribeBlock extends Component {
 
     render() {
         return (
+            <>
             <div className="form-subscribe">
-                    <div className="bg-pic">
+                <div className="bg-pic">
 
-                        <div className="bgleft"></div>
-                        <div className="bg-form">
+                    <div className="bgleft"></div>
+                    <div className="bg-form">
 
-                            <h2>Înscrieți-vă pentru a colecta artă de la cei mai importanți artiști din Moldova</h2>
-                            <div className="subscribe-button">
-                               <a href='/login'><button className="btn-conect"> 
-                                    conectare
-                                </button></a>
-                               <a href='/signup'> <button className="btn-registr"> 
-                                    registrare
-                                </button></a>
-                            </div>
+                        <h2>Înscrieți-vă pentru a colecta artă de la cei mai importanți artiști din Moldova</h2>
+                        <div className="subscribe-button">
+                            <a href='/login'><button >
+                                conectare
+                            </button></a>
+                            <a href='/signup'> <button id='SignupBtn' className="btn-registr">
+                                registrare
+                            </button></a>
                         </div>
-                        <div className="bgright"></div>
                     </div>
-
-
+                    <div className="bgright"></div>
                 </div>
-        );
-    }
-}
 
-export default SubscribeBlock;
+
+            </div>
+            </>
+        );
+    };
+};
