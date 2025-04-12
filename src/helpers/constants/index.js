8f3b5959ba80e7792fa3598f310de373fe8e9311
Vladlena <cravcenco97@inbox.ru>
2025-04-12 18:26:18 +0300

    #

--- src/helpers/constants/index.js
+++ src/helpers/constants/index.js
@@ -3,5 +3,5 @@ export const API_URL = "https://your-api.onrender.com/api";
 export const GET_PRODUCTS_LIST = API_URL + "/gallery";
 export const SET_LIKE = API_URL + "/like";
 
-export const NO_IMAGE_URL = 'https://via.placeholder.com/300x200';
+export const NO_IMAGE_URL = "https://via.placeholder.com/300x200";
 export const ONE_PRODUCT_EMPTY_DATA = {};
