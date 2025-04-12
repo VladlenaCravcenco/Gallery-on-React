ca0c09ff8817108023b352e5dbffe82fe5b7e486
Vladlena <cravcenco97@inbox.ru>
2025-04-12 12:36:02 +0300

    #

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+import React from 'react';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
+
+function Home() {
+  return <h2>Главная</h2>;
+}
+
+function About() {
+  return <h2>О проекте</h2>;
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <HashRouter>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </div>
+    </HashRouter>
   );
 }
 
--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,60 +1,47 @@
 import React, { useEffect } from 'react';
 
 import MainPage from './mainpage.js';
-
 import MyRoom from './myroom.js';
 import RandomProf from './randomprofile.js';
 import Setings from './Setings.js';
-
-import TablouriPopulare from './tablouri-populare.js'
-import Desprenoi from './desprenoi.js'
-import Evenimente from './evenimente.js'
-import Licitatie from './licitatie.js'
-import Artistii from './artistii'
-import Contacte from './contacte'
+import TablouriPopulare from './tablouri-populare.js';
+import Desprenoi from './desprenoi.js';
+import Evenimente from './evenimente.js';
+import Licitatie from './licitatie.js';
+import Artistii from './artistii';
+import Contacte from './contacte';
 
 import './app.css';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { HashRouter, Route, Routes } from 'react-router-dom';
 import CookieHelper from "../../helpers/cookie-helper";
-import { setToken } from "../../helpers/constants/functions"
-//"fsevents": "^2.3.2",
+import { setToken } from "../../helpers/constants/functions";
+
 const App = () => {
+  useEffect(() => {
+    const token = CookieHelper("get", "token");
+    const generateToken = setToken();
 
-    useEffect(() => {
+    if (!token) {
+      CookieHelper("set", "token", generateToken);
+    }
+  }, []);
 
-        const token = CookieHelper("get", "token");
-        const generateToken = setToken()
-
-        if (!token) {
-            CookieHelper("set", "token", generateToken);
-        }
-    },[])
-
-    return (
-        <>
-            <BrowserRouter>
-
-                <Routes>
-                    <Route path='/' Component={MainPage} />
-
-                    <Route path='/myroom' Component={MyRoom} />
-                    <Route path='/random' Component={RandomProf} />
-                    <Route path='/setings' Component={Setings} />
-
-                    <Route path='/tablouripopulare' Component={TablouriPopulare} />
-                    <Route path='/desprenoi' Component={Desprenoi} />
-                    <Route path='/evenimente' Component={Evenimente} />
-                    <Route path='/licitatie' Component={Licitatie} />
-                    <Route path='/artistii' Component={Artistii} />
-                    <Route path='/contacte' Component={Contacte} />
-
-                </Routes>
-
-
-            </BrowserRouter>
-
-        </>
-    )
-
+  return (
+    <HashRouter>
+      <Routes>
+        <Route path='/' Component={MainPage} />
+        <Route path='/myroom' Component={MyRoom} />
+        <Route path='/random' Component={RandomProf} />
+        <Route path='/setings' Component={Setings} />
+        <Route path='/tablouripopulare' Component={TablouriPopulare} />
+        <Route path='/desprenoi' Component={Desprenoi} />
+        <Route path='/evenimente' Component={Evenimente} />
+        <Route path='/licitatie' Component={Licitatie} />
+        <Route path='/artistii' Component={Artistii} />
+        <Route path='/contacte' Component={Contacte} />
+      </Routes>
+    </HashRouter>
+  );
 };
+
 export default App;
